09b72fcd374393418edc3e8047bc72e64d27a7e9
a-cola <cola.andrea.2000@gmail.com>
2024-04-07 17:16:59 +0200

    Aggiunta Error Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { StartPageComponent } from './components/start-page/start-page.component';
 import { ChatPageComponent } from './components/chat-page/chat-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 
 export const routes: Routes = [
+    {path:'', redirectTo:'chatlist', pathMatch:'full'},
     {path:'chatlist', component:StartPageComponent},
     {path:'chatlist/:id', component:ChatPageComponent},
-    {path: '', redirectTo:'chatlist', pathMatch:'full'},
+    {path:'**', pathMatch:'full', component:ErrorPageComponent}
 ];
